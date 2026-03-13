54cf6c71d7adfd23e3bdd60c9b37ff39c244444b
fabgalan-p1 <78654883+areporotas@users.noreply.github.com>
2026-03-13 20:26:43 +0100

    Add setup.py for super_package configuration

---
+++ super_package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='super_package',
+    version='1.0',
+    packages=['pandas'],
+    url='',
+    license='',
+    author='super_moi',
+    author_email='',
+    description=''
+)
